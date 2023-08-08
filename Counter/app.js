5e4b88238237cfec2b911a97aede0bdfd34151e7
andrewcyi <andrewcyi94@gmail.com>
2023-08-07 23:57:11 -0400

    Counter Application: Added ability to change increments. Bugfix required
    Updating functionality of counter to allow for different increments in the counter.
    Need to fix bug that just concatenates the value instead of mathematically adding it together.

--- Counter/app.js
+++ Counter/app.js
@@ -1,20 +1,31 @@
 const numberValue = document.getElementById('value');
 const btns = document.querySelectorAll('.btn');
+const incrementInput = document.querySelector('input');
 let count = 0;
+let increment = 1;
 
 console.log(btns);
+
+const incrementNumberUpdate = () => {
+    if(incrementInput.value === 0 ){
+        increment = 1;
+    }
+    increment = incrementInput.value;
+}
+
+incrementInput.addEventListener('input',incrementNumberUpdate);
 
 btns.forEach(btn => {
     btn.addEventListener('click', (e) =>{
         const styles = e.currentTarget.classList;
         if(styles.contains('decrease')){
-            count--;
+            count = count - increment;
         }
         else if(styles.contains('reset')){
             count = 0;
         }
         else if(styles.contains('increase')){
-            count++;
+            count = count + increment;
         }
         numberValue.textContent = count;
         valueChecker();
@@ -32,3 +43,4 @@ const valueChecker = () => {
         numberValue.style.color = 'green';
     }
 }
+
